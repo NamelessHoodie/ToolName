5b659a0928e5eb38a4ec19ed607cec94d4c73436
NamelessHoodie <73077577+NamelessHoodie@users.noreply.github.com>
2021-08-17 05:57:38 -0700

    Implemented custom window layout and MVVM file structure

---
+++ HoodieSuite/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoodieSuite.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+    }
+}
--- HoodieSuite/MainWindow.xaml.cs
+++ HoodieSuite/MainWindow.xaml.cs
@@ -24,5 +24,31 @@ namespace HoodieSuite
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
